66ffd9be22f3572e24ba34410c9b55f49ca4b2b1
Penthai SRIRUANG <penthai.sriruang@gmail.com>
2022-06-20 13:27:35 +0200

    Ajout de la vue module

---
+++ Views/module.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../Ressources/style.css" media="screen" type="text/css" />
+        <title>Modules</title>
+    </head>
+    <body>
+        <h1>Liste des modules</h1>
+    </body>
+</html>
