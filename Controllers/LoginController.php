cf56d6e2fa5699f723437ab81fa0e37802dcf142
Penthai SRIRUANG <penthai.sriruang@gmail.com>
2022-06-20 13:24:29 +0200

    MAJ LoginController + Ajout ModuleController

---
+++ Controllers/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Controllers;
+
+use Models\User;
+
+class LoginController {
+
+
+    public function Login()
+    {
+        //Charge les models
+        $oUserModel = new User();
+    }
+}
---
+++ Controllers/ModuleController.php
@@ -0,0 +1,8 @@
+<?php
+namespace Controllers;
+
+class ModuleController {
+    public function show() {
+        // Méthode permettant d'afficher les différentes modules des plantes
+    }
+}
